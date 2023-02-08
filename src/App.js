3877971feb9d8a7db4049f3f45ce8fe1c09e53c0
polimaurizio <maurizio.poli@artigianelli.tn.it>
2023-02-08 19:03:26 +0100

    NavBar fix

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import Container from "./Components/Container";
 import {useEffect} from "react";
 import Card from "./Components/Card";
 import Nav from './Components/Nav'
+import Button from './Components/Button'
 
 function App() {
     /*useEffect(()=>{
         document.getElementsByTagName('html')[0].classList.add('dark');
     }, []);*/
-
   return (
-    <div>
-
       <Nav />
-
-        {/*<Container className={'place-items-center h-screen'}>
-          <div className={'items-center justify-center flex h-screen'}>
-              <Card />
-          </div>
-        </Container>*/}
-        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
-    </div>
   );
 }
 
--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import classNames from "classnames";
 
+const Button = ({children, className}) => {
+    let classes = classNames('', className);
 
-const Button = ({children}) => {
     return(
-        <button type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        <button className={classes}>
             {children}
         </button>
-    );
+    )
 };
 
 export default Button;
