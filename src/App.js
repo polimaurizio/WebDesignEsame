f5e7f4194eaafceb545da8aa547ddfc63bb2e162
polimaurizio <maurizio.poli@artigianelli.tn.it>
2023-02-08 12:57:21 +0100

    Dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Container from "./Components/Container";
-import Button from './Components/Button';
+import Card from "./Components/Card"
+import {useEffect} from "react";
 
 function App() {
+    useEffect(()=>{
+        document.getElementsByTagName('html')[0].classList.add('dark');
+    }, []);
+
   return (
-      <Container className={'flex'}>
-        <Button>Prova</Button>
+      <Container className={'place-items-center h-screen'}>
+          <div className={'items-center justify-center flex h-screen'}>
+            <Card />
+          </div>
       </Container>
   );
 }
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({}) => {
+    return(
+        <div>
+            <a href="#"
+               className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Esame Web Design 2023</h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">Stiamo lavorando per rendere questa pagina disponibile al più presto!</p>
+            </a>
+        </div>
+    );
+}
+
+export default Card
