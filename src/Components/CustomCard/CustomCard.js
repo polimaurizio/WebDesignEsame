27a0a16618b1b04b639f8d81d918c3f527d39a59
polimaurizio <maurizio.poli@artigianelli.tn.it>
2023-02-14 23:14:15 +0100

    Resize CustomCard problem semi fixed

--- src/Components/CustomCard/CustomCard.js
+++ src/Components/CustomCard/CustomCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {Card} from 'flowbite-react';
+import CustomButton from '../CustomButton';
+
 import '../../App.css';
 
 const CustomCard = ({}) => {
-    return(
-        <div className={'sm:flex sm:justify-between mx-10 sm:place-content-center text-montserrat font-light'}>
+    return (
+        <div>
+            <div
+                className={'sm:flex justify-between md:justify-center mx-10 sm:place-content-center text-montserrat font-light md:text-sm sm:text-xs text-sm'}>
+
+
                 <div className="max-w-sm mt-20 sm:mx-4 mx-auto">
                     <Card imgSrc="/Loghi/image_1.jpeg" className={'h-full'}>
                         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -12,36 +18,61 @@ const CustomCard = ({}) => {
                         </h5>
                         <p className="font-normal text-gray-700 dark:text-gray-400 font-montserrat font-normal">
                             Il nuovo modello di punta delle macchine a leva domestiche.
-                            Materiali pregiati e possibilità di controllo costante della pressione di erogazione esercitata abbassando...
+                            Materiali pregiati e possibilità di controllo costante della pressione di erogazione
+                            esercitata
+                            abbassando...
                         </p>
                     </Card>
                 </div>
 
-                <div className="max-w-sm mt-20 sm:mx-4 mx-auto" >
-                    <Card imgSrc="/Loghi/image_3.jpeg" className={'h-full'}>
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            Esperto Abile
-                        </h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400 font-montserrat font-normal">
-                            Il nuovo modello di punta delle macchine a leva domestiche.
-                            Materiali pregiati e possibilità di controllo costante della pressione di erogazione esercitata abbassando la...
-                        </p>
-                    </Card>
-                </div>
 
                 <div className="max-w-sm mt-20 sm:mx-4 mx-auto">
-                    <Card imgSrc="/Loghi/image_2.jpeg" className={'h-full'}>
+                    <Card imgSrc="/Loghi/image_1.jpeg" className={'h-full'}>
                         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            Esperto Competenze
+                            Esperto Edotto
                         </h5>
                         <p className="font-normal text-gray-700 dark:text-gray-400 font-montserrat font-normal">
                             Il nuovo modello di punta delle macchine a leva domestiche.
-                            Materiali pregiati e possibilità di controllo costante della pressione di erogazione esercitata abbassando la...
+                            Materiali pregiati e possibilità di controllo costante della pressione di erogazione
+                            esercitata
+                            abbassando...
                         </p>
                     </Card>
                 </div>
-        </div>
 
+
+                <div className="max-w-sm mt-20 sm:mx-4 mx-auto">
+                    <Card imgSrc="/Loghi/image_1.jpeg" className={'h-full'}>
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Esperto Edotto
+                        </h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400 font-montserrat font-normal">
+                            Il nuovo modello di punta delle macchine a leva domestiche.
+                            Materiali pregiati e possibilità di controllo costante della pressione di erogazione
+                            esercitata
+                            abbassando...
+                        </p>
+                    </Card>
+                </div>
+
+
+                <div className="max-w-sm mt-20 sm:mx-4 mx-auto sm:hidden md:block">
+                    <Card imgSrc="/Loghi/image_1.jpeg" className={'h-full'}>
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Esperto Edotto
+                        </h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400 font-montserrat font-normal">
+                            Il nuovo modello di punta delle macchine a leva domestiche.
+                            Materiali pregiati e possibilità di controllo costante della pressione di erogazione
+                            esercitata
+                            abbassando...
+                        </p>
+                    </Card>
+                </div>
+            </div>
+
+            <CustomButton className={''}>Scopri di più!</CustomButton>
+        </div>
     );
 }
 
