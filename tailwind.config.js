7c5ed6e07917ad98db3b076a4b84ab65fe1f09d8
polimaurizio <maurizio.poli@artigianelli.tn.it>
2023-03-10 16:01:23 +0100

    Tailwind remove dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false,
   content:
       ["./src/**/*.{html,js}",
         "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
